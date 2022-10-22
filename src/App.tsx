3a2c90fef63e2e08d55a08506a85f00c95675637
Fabricio <fabriciopolicarpo0@gmail.com>
2022-10-22 23:08:09 +0000

    working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState, useMemo, useCallback } from "react";
 import { Pokemon, getAll, getByName } from "./API";
-
 import "./styles.css";
 
 const calculatePower = (pokemon: Pokemon) =>
@@ -11,8 +10,12 @@ const calculatePower = (pokemon: Pokemon) =>
   pokemon.special_defense +
   pokemon.speed;
 
+interface PokemonwithPower extends Pokemon {
+  power: number;
+}
+
 const PokemonTable: React.FunctionComponent<{
-  pokemon: Pokemon[];
+  pokemon: PokemonwithPower[];
 }> = ({ pokemon }) => {
   return (
     <table>
@@ -22,6 +25,7 @@ const PokemonTable: React.FunctionComponent<{
           <td>Name</td>
           <td>Type</td>
           <td colSpan={6}>Stats</td>
+          <td> Power </td>
         </tr>
       </thead>
       <tbody>
@@ -36,6 +40,7 @@ const PokemonTable: React.FunctionComponent<{
             <td>{p.special_attack}</td>
             <td>{p.special_defense}</td>
             <td>{p.speed}</td>
+            <td>{p.power}</td>
           </tr>
         ))}
       </tbody>
@@ -44,20 +49,58 @@ const PokemonTable: React.FunctionComponent<{
 };
 
 export default function App() {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+  const [threshold, setThreshold] = useState(0);
+  const [search, setSearch] = useState("");
+
+  function handleThreshold(event: React.ChangeEvent<HTMLInputElement>) {
+    setThreshold(Number(event.target.value));
+  }
+
+  useEffect(() => {
+    getByName(search).then(setPokemon);
+  }, [search]);
+
+  const pokemonWithPower = useMemo(() => {
+    return pokemon.map((pk) => ({
+      ...pk,
+      power: calculatePower(pk),
+    }));
+  }, [pokemon]);
+
+  const countOverThreshold = useMemo(() => {
+    return pokemonWithPower.filter((pk) => pk.power > threshold).length;
+  }, [pokemonWithPower, threshold]);
+
+  const onSetSearch = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSearch(event.target.value);
+    },
+    []
+  );
+
+  const min = useMemo(() => {
+    return Math.min(...pokemonWithPower.map((p) => p.power));
+  }, [pokemonWithPower]);
+
+  const max = useMemo(() => {
+    return Math.max(...pokemonWithPower.map((p) => p.power));
+  }, [pokemonWithPower]);
+
   return (
     <div>
       <div className="top-bar">
         <div>Search</div>
-        <input type="text"></input>
-        <div>Power threshold</div>
-        <input type="text"></input>
-        <div>Count over threshold: </div>
+        <input type="text" onChange={onSetSearch} value={search} />
+        <div>Power threshold: {threshold}</div>
+        <input value={threshold} type="text" onChange={handleThreshold}></input>
+        <div>Count over threshold: {countOverThreshold}</div>
       </div>
       <div className="two-column">
-        <PokemonTable pokemon={[]} />
+        <PokemonTable pokemon={pokemonWithPower} />
         <div>
-          <div>Min: </div>
-          <div>Max: </div>
+          <div>Min: {min}</div>
+          <div>Max: {max}</div>
         </div>
       </div>
     </div>
